8f93db9fc8ee7313de553cd242b57b87bf824007
hosik <Hoseinsniper32@gmail.com>
2025-09-13 18:07:49 +0200

    Extra uppgift för split.py

--- src/kmom01/split_bill/split.py
+++ src/kmom01/split_bill/split.py
@@ -2,9 +2,8 @@
 x = int(input("Vad är summan på notan?\n"))
 y = int(input("Hur många personer ska dela på nota?\n"))
 
-z = x/y
-
-print(f"Varje person ska betala {z}")
-
-
-# Andra push för extra uppgift
+if y <= 0:
+    print("Varning! Antal personer måste vara minst 1.")
+else:
+    z = x/y
+    print(f"Varje person ska betala {z}")
